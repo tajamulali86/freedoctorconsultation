551d2405091bb359af847e72914207b0c4ad66d4
Tajamul <tajamulali86@gmail.com>
2023-07-11 17:58:26 +0530

    eddited, edit to add

--- src/components/postapi.js
+++ src/components/postapi.js
@@ -1,42 +1,42 @@
 "use client"
-
 import { useState } from 'react';
 
 function AddtoApi() {
   const [formData, setFormData] = useState({
     name: '',
     email: '',
-    role:'',
+    role: '',
   });
-  
 
   const handleInputChange = (e) => {
     setFormData({
       ...formData,
-      [e.target.name]: e.target.value
+      [e.target.name]: e.target.value,
     });
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-  
 
     try {
-      const response = await fetch(`http://localhost:8000/api/patients`, {
+      const url = `http://localhost:8000/api/${formData.role}`;
+      const response = await fetch(url, {
         method: 'POST',
         headers: {
-          'Content-Type': 'application/json'
+          'Content-Type': 'application/json',
         },
-        body: JSON.stringify(formData)
+        body: JSON.stringify(formData),
       });
 
       if (response.ok) {
         // Data added successfully
-        // Perform any necessary actions or show a success messeage
+        // Perform any necessary actions or show a success message
         console.log('Data added successfully');
-        empty=()=>setFormData({
-          name: "",email:""
-        })
+        setFormData({
+          name: '',
+          email: '',
+          role: '',
+        });
       } else {
         // Handle error case
         console.log('Failed to add data');
@@ -48,30 +48,66 @@ function AddtoApi() {
 
   return (
     <div className='container px-10 py-0 mx-auto mt-10 flex flex-wrap items-center'>
-      <form onSubmit={handleSubmit} className=" bg-gray-100 rounded-lg p-8 flex flex-col  w-1/3 mt-10 md:mt-0 mx-20">
+      <form
+        onSubmit={handleSubmit}
+        className='bg-gray-100 rounded-lg p-8 flex flex-col w-1/3 mt-10 md:mt-0 mx-20'
+      >
         <label className='leading-7 text-sm text-gray-600'>
           Name:
-          <input type="text" name="name" value={formData.name} onChange={handleInputChange}  className='w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out'/>
+          <input
+            type='text'
+            name='name'
+            value={formData.name}
+            onChange={handleInputChange}
+            className='w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out'
+          />
         </label>
         <br />
 
         <label className='leading-7 text-sm text-gray-600'>
           Email:
-          <input type="text" name="email" value={formData.email} onChange={handleInputChange}  className='w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out'/>
+          <input
+            type='text'
+            name='email'
+            value={formData.email}
+            onChange={handleInputChange}
+            className='w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out'
+          />
         </label>
-     
+
         <br />
-        <label className='leading-7 text-sm text-gray-600'>
-Doctor or Patient        
-        </label>
+        <label className='leading-7 text-sm text-gray-600'>Doctor or Patient</label>
         <div>
-         <input type="radio" id="html" name="role" value="doctors" className='w-1/2 bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out'/>
- <label htmlFor="html">Doctor</label><br/>
- <input type="radio" id="css" name="role" value="patients" className='w-1/2 bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out'/>
- <label htmlForfor="css">Patient</label><br/>
-     <br/>
-     </div>
-        <button type="submit" className='text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg' >Add Data</button>
+          <input
+            type='radio'
+            id='doctors'
+            name='role'
+            value='doctors'
+            checked={formData.role === 'doctors'}
+            onChange={handleInputChange}
+            className='w-1/2 bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out'
+          />
+          <label htmlFor='doctors'>Doctor</label>
+          <br />
+          <input
+            type='radio'
+            id='patients'
+            name='role'
+            value='patients'
+            checked={formData.role === 'patients'}
+            onChange={handleInputChange}
+            className='w-1/2 bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out'
+          />
+          <label htmlFor='patients'>Patient</label>
+          <br />
+          <br />
+        </div>
+        <button
+          type='submit'
+          className='text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg'
+        >
+          Add Data
+        </button>
       </form>
     </div>
   );
