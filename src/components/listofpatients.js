1a4852070186d57ecda6f6294db25ecd7d1beff3
Tajamul <tajamulali86@gmail.com>
2023-07-14 18:34:56 +0530

    added pagination to list

--- src/components/listofpatients.js
+++ src/components/listofpatients.js
@@ -1,17 +1,13 @@
 "use client"
-import download  from 'downloadjs';
-import Delete from './deletemodal';
-import Editinfo from './editmodal';
 import Link from 'next/link';
-import { unstable_serialize } from 'swr'
-import { unstable_serialize as infinite_unstable_serialize } from 'swr/infinite'
-import useSWR from "swr"
-import Image from 'next/image';
+import useSWR from "swr";
 import Spinner from './spinner';
+import { useState } from 'react';
 
-const fetcher = (...args) => fetch(...args).then(res => res.json())
 
-function useUser({role}) {
+const fetcher = (...args) => fetch(...args).then(res => res.json());
+
+function useUser({ role }) {
   const { data, error, isLoading } = useSWR(`http://localhost:8000/api/${role}`, fetcher)
 
   return {
@@ -21,111 +17,35 @@ function useUser({role}) {
   }
 }
 
+function PatientList({ role }) {
 
-function PatientList({role}) {
-  const handleDelete = (id) => {
-    // Filter out the deleted patient from the patients array
-    const updatedPatients = data.filter((item) => item.id !== id);
-    setData(updatedPatients);
-    const [data, setData] = useState(null);
-  }
-  const { user, isLoading, isError } = useUser({role})
+  const [page, setPage] = useState(1);
+  const perPage = 10;
 
-  // const [patients, setPatients] = useState([]);
+  const { user, isLoading, isError } = useUser({ role })
 
-  if (isLoading) return (<div>
+  if (isLoading) return (<Spinner />);
 
-    {/* <div className="relative overflow-x-auto animate-pulse ">
-      <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-        <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-          <tr>
-            <th scope="col" className="px-6 py-3">
-               name
-            </th>
-            <th scope="col" className="px-6 py-3">
-              Color
-            </th>
-            <th scope="col" className="px-6 py-3">
-              Category
-            </th>
-            <th>
-              Status
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-        
-
-            <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-              <th
-                scope="row"
-                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-              >
-                id
-              </th>
-              <td className="px-6 py-4">Name</td>
-              <td className="px-6 py-4">Email</td>
-              <td className="px-6 py-4">
-
-              
-              
-                </td>
-            </tr>
-            <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-              <th
-                scope="row"
-                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-              >
-                id
-              </th>
-              <td className="px-6 py-4">Name</td>
-              <td className="px-6 py-4">Email</td>
-              <td className="px-6 py-4">
-
-              
-              
-                </td>
-            </tr>
-            <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-              <th
-                scope="row"
-                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-              >
-                id
-              </th>
-              <td className="px-6 py-4">Name</td>
-              <td className="px-6 py-4">Email</td>
-              <td className="px-6 py-4">
-
-              
-              
-                </td>
-            </tr>
-        </tbody>
-      </table>
-    </div> */}
-<Spinner/>
-
-
-  </div>);
   if (isError) return ("errr");
-  // return <img src={user.avatar} />
+
+  const startIndex = (page - 1) * perPage;
+  const endIndex = startIndex + perPage;
+  const displayedUsers = user.slice(startIndex, endIndex);
 
   return (
-    <div>
-
+    <>
       <div className="container mx-auto ">
         <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
           <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
             <tr>
               <th scope="col" className="px-6 py-3">
-                 ID
+                ID
               </th>
               <th scope="col" className="px-6 py-3">
                 Name
               </th>
               <th scope="col" className="px-6 py-3">
-                 Picture
+                Picture
               </th>
               <th scope="col" className="px-6 py-3">
                 Email
@@ -133,30 +53,27 @@ function PatientList({role}) {
               <th scope="col" className="px-6 py-3">
                 File
               </th>
-              {role=="doctors"?<>
-              <th scope="col" className="px-6 py-3">
-                Phone
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Degree
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Experiance
-              </th>
-             </>
-                :''}
-             
+              {role == "doctors" ? <>
+                <th scope="col" className="px-6 py-3">
+                  Phone
+                </th>
+                <th scope="col" className="px-6 py-3">
+                  Degree
+                </th>
+                <th scope="col" className="px-6 py-3">
+                  Experiance
+                </th>
+              </>
+                : ''}
+
               <th>
                 Status
               </th>
             </tr>
           </thead>
           <tbody>{
-            user.map((item) => 
-           
+            displayedUsers .map((item) =>
             (
-              
-
               <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700" key={item.id} onDelete={item.className = "hidden"}>
                 <th
                   scope="row"
@@ -165,79 +82,83 @@ function PatientList({role}) {
                   {item.id}
                 </th>
                 <td className="px-6 py-4">{item.name}</td>
-                <td className="px-6 py-4"> 
-                
-                {item.profileimg?
-                 <a
-    href={`http://localhost:8000/storage/${item.profileimg}`}
-   target='_blank'
-  >
-   <img
-  src={`http://localhost:8000/storage/${item.profileimg}`}
-  width={100}
-  height={100}
-  alt='profile'/>
-  </a>:
-   <a
-   href={`/images/images.png`}
-  target='_blank'
- >
-  <img
- src={`/images/images.png`}
- width={100}
- height={100}
- alt='profile'/>
- </a>
-}
-  </td>
+                <td className="px-6 py-4">
+
+                  {item.profileimg ?
+                    <a
+                      href={`http://localhost:8000/storage/${item.profileimg}`}
+                      target='_blank'
+                    >
+                      <img
+                        src={`http://localhost:8000/storage/${item.profileimg}`}
+                        width={100}
+                        height={100}
+                        alt='profile' />
+                    </a> :
+                    <a
+                      href={`/images/images.png`}
+                      target='_blank'
+                    >
+                      <img
+                        src={`/images/images.png`}
+                        width={100}
+                        height={100}
+                        alt='profile' />
+                    </a>
+                  }
+                </td>
                 <td className="px-6 py-4">{item.email}</td>
                 <td className="px-6 py-4">
-  <a
-    href={`http://localhost:8000/storage/${item.file_path}`}
-   target='_blank'
-  >
-  <button className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800">
-                    <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
-                     View  file
-                    </span>
-                  </button>
-  </a>
-</td>
-                {role=="doctors"?<>
-                <td className="px-6 py-4">{item.phone}</td>
-                <td className="px-6 py-4">{item.degree}</td>
-                <td className="px-6 py-4">{item.experiance}</td></>
-                :''}
+                  <a
+                    href={`http://localhost:8000/storage/${item.file_path}`}
+                    target='_blank'
+                  >
+                    <button className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800">
+                      <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+                        View  file
+                      </span>
+                    </button>
+                  </a>
+                </td>
+                {role == "doctors" ? <>
+                  <td className="px-6 py-4">{item.phone}</td>
+                  <td className="px-6 py-4">{item.degree}</td>
+                  <td className="px-6 py-4">{item.experiance}</td></>
+                  : ''}
                 <td className="py-4">
 
                   <Link href={`${role}/${item.id}`}> <button className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800">
                     <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
-                      {role=="patients"?"Consult":"More information"}
+                      {role == "patients" ? "Consult" : "More information"}
                     </span>
                   </button></Link>
-                  {/* || */}
-                  {/* <button className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
-                    <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
-                      Edit
-                    </span>
-                  </button> */}
-                  {/* <Editinfo/> */}
-                  {/* || */}
-                  {/* <button      className= "relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-pink-500 to-orange-400 group-hover:from-pink-500 group-hover:to-orange-400 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800">
-  <span  className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
-      Delete
-  </span>
-</button> */}
-                  {/* <Delete name={item.name} id={item.id} onDelete={handleDelete} /> */}
-                  </td>
+    
+                </td>
               </tr>))}
           </tbody>
         </table>
+        
       </div>
+      <div className="flex justify-center my-4">
+        {page > 1 && (
+          <button
+            onClick={() => setPage(page - 1)}
+            className="px-4 py-2 mr-2 text-sm font-medium text-gray-900 bg-white border border-gray-300 rounded-md hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+          >
+            Previous
+          </button>
+        )}
 
-
-
-    </div>
+        {user.length > endIndex && (
+          <button
+            onClick={() => setPage(page + 1)}
+            className="px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-300 rounded-md hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+          >
+            Next
+          </button>
+        )}
+      </div>
+    </>
   );
 }
 
